444657e1e88a880be8107c0fc870b9850c8399fe
Victor Nicacio <victorcnicacio@hotmail.com>
2025-11-16 00:04:36 -0300

    feat: preferred theme is saved on storage

--- src/providers/themeProvider.tsx
+++ src/providers/themeProvider.tsx
@@ -1,4 +1,11 @@
-import React, { createContext, ReactNode, useContext, useState } from "react";
+import { getItem, saveItem } from "@/hooks/useAsyncStorage";
+import React, {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { useColorScheme as _useColorScheme } from "react-native";
 
 type Theme = "light" | "dark";
@@ -14,8 +21,31 @@ export function ThemeProvider({ children }: { children: ReactNode }) {
   const systemTheme = _useColorScheme() as Theme;
   const [theme, setTheme] = useState<Theme>(systemTheme ?? "light");
 
-  const toggleTheme = () => {
-    setTheme((prev) => (prev === "light" ? "dark" : "light"));
+  useEffect(() => {
+    const loadPreferredTheme = async () => {
+      try {
+        const savedTheme = await getItem({ key: "preferredTheme" });
+        if (savedTheme) {
+          const parsedTheme = JSON.parse(savedTheme) as Theme;
+          setTheme(parsedTheme);
+        }
+      } catch (error) {
+        console.error("Falha ao carregar o tema:", error);
+      }
+    };
+
+    loadPreferredTheme();
+  }, []);
+
+  const toggleTheme = async () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+
+    try {
+      await saveItem({ key: "preferredTheme", value: newTheme });
+      setTheme(newTheme);
+    } catch (error) {
+      console.error("Falha ao salvar o tema:", error);
+    }
   };
 
   return (
@@ -27,6 +57,7 @@ export function ThemeProvider({ children }: { children: ReactNode }) {
 
 export function useThemeSwitcher() {
   const ctx = useContext(ThemeContext);
-  if (!ctx) throw new Error("useThemeSwitcher deve ser usado dentro de ThemeProvider");
+  if (!ctx)
+    throw new Error("useThemeSwitcher deve ser usado dentro de ThemeProvider");
   return ctx;
 }
