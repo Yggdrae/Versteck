e398c375a4dbed9250b5aad164e1302ed660f8db
Victor Nicacio <victorcnicacio@hotmail.com>
2025-11-18 00:07:30 -0300

    fix: safe area bottom edge

--- src/components/ThemedSafeArea.tsx
+++ src/components/ThemedSafeArea.tsx
@@ -2,20 +2,11 @@ import { ReactNode } from "react";
 import { useTheme } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-export function ThemedSafeAreaView({
-  style,
-  children,
-}: {
-  style?: any;
-  children: ReactNode;
-}) {
+export function ThemedSafeAreaView({ style, children, edges }: { style?: any; children: ReactNode; edges?: string[] }) {
   const { colors } = useTheme();
 
   return (
-    <SafeAreaView
-      style={[{ flex: 1, backgroundColor: colors.background }, style]}
-      edges={["top"]}
-    >
+    <SafeAreaView style={[{ flex: 1, backgroundColor: colors.background }, style]} edges={["top", "bottom"]}>
       {children}
     </SafeAreaView>
   );
