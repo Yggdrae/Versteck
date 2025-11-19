186683f05952503ce37a208853a07332a645be89
Victor Nicacio <victorcnicacio@hotmail.com>
2025-11-19 00:45:03 -0300

    feat: api calls on master-key change

--- src/hooks/useChangeMasterKey.ts
+++ src/hooks/useChangeMasterKey.ts
@@ -1,4 +1,5 @@
 import { useUser } from "@/providers/userContext";
+import { api } from "@/service/api";
 import { decryptVault, EncryptedPayload, encryptVault } from "@/service/encrypt";
 import { readVaultFile, writeVaultFile } from "@/service/storage";
 
@@ -6,7 +7,7 @@ export const useChangeMasterKey = () => {
   const { setMasterKey } = useUser();
 
   const changeKey = async (oldKey: string, newKey: string) => {
-    console.log("🔐 Iniciando troca de Chave Mestra...");
+    console.log("Iniciando troca de Chave Mestra...");
 
     try {
       const content = await readVaultFile();
@@ -17,19 +18,25 @@ export const useChangeMasterKey = () => {
       }
 
       const payload: EncryptedPayload = JSON.parse(content);
-
       const currentVaultData = await decryptVault(oldKey, payload);
 
-      console.log("🔓 Cofre aberto. Re-criptografando com nova chave...");
+      console.log("Criptografando com nova chave...");
 
       const newPayload = await encryptVault(newKey, currentVaultData);
 
       const fileString = JSON.stringify(newPayload);
       await writeVaultFile(fileString);
 
+      await api.put("/vault", {
+          vaultData: newPayload.encryptedData,
+          vaultIv: newPayload.iv,
+          vaultTag: newPayload.tag,
+          kdfSalt: newPayload.salt
+      });
+
       setMasterKey(newKey);
 
-      console.log("Chave Mestra alterada e Contexto atualizado!");
+      console.log("Chave Mestra alterada e sincronizada!");
       return true;
     } catch (error) {
       console.error("Falha ao alterar chave mestra:", error);
