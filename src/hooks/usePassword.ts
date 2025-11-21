7e8a48384472609332a25848dae0d7437f67342e
Victor Nicacio <victorcnicacio@hotmail.com>
2025-11-21 01:40:59 -0300

    refactor: API calls on create, edit and delete password

--- src/hooks/usePassword.ts
+++ src/hooks/usePassword.ts
@@ -4,14 +4,15 @@ import { api } from "@/service/api";
 import { decryptVault, EncryptedPayload, encryptVault } from "@/service/encrypt";
 import { readVaultFile, writeVaultFile } from "@/service/storage";
 import { useQuery } from "@tanstack/react-query";
+import { Buffer } from "buffer";
 
-const syncVaultWithServer = async (payload: EncryptedPayload) => {
+export const syncVaultWithServer = async (payload: EncryptedPayload) => {
   try {
-    await api.put("/vault", {
-      vaultData: payload.encryptedData,
-      vaultIv: payload.iv,
+    await api.put("/vaultdata", {
+      encryptedBlob: payload.encryptedData,
+      vaultIV: payload.iv,
       vaultTag: payload.tag,
-      kdfSalt: payload.salt,
+      //kdfSalt: payload.salt,
     });
     console.log("Cofre sincronizado com a nuvem!");
   } catch (error) {
@@ -33,7 +34,10 @@ export const useLoadPasswords = () => {
 
       try {
         const payload: EncryptedPayload = JSON.parse(content);
-        const vaultData = await decryptVault(masterKey, payload);
+
+        const masterKeyBuffer = Buffer.from(masterKey, "base64");
+
+        const vaultData = await decryptVault(masterKeyBuffer, payload);
         return vaultData as IPassword[];
       } catch (error) {
         console.error("Falha ao carregar senhas:", error);
@@ -48,18 +52,23 @@ export const useSavePassword = async (masterKey: string, newPassword: IPassword)
   try {
     const content = await readVaultFile();
     let currentVault: IPassword[] = [];
+    let currentSalt: string | undefined;
+
+    const masterKeyBuffer = Buffer.from(masterKey, "base64");
 
     if (content) {
       const payload: EncryptedPayload = JSON.parse(content);
-      currentVault = await decryptVault(masterKey, payload);
+      currentSalt = payload.salt;
+      currentVault = await decryptVault(masterKeyBuffer, payload);
+    } else {
+      throw new Error("Cofre não inicializado. Faça login novamente.");
     }
 
     currentVault.push(newPassword);
 
-    const newPayload = await encryptVault(masterKey, currentVault);
+    const newPayload = await encryptVault(masterKeyBuffer, currentVault, currentSalt);
 
     await writeVaultFile(JSON.stringify(newPayload));
-
     await syncVaultWithServer(newPayload);
 
     return true;
@@ -72,17 +81,24 @@ export const useSavePassword = async (masterKey: string, newPassword: IPassword)
 export const useEditPassword = async (masterKey: string, updatedItem: IPassword) => {
   try {
     const content = await readVaultFile();
-    if (!content) throw new Error("Cofre vazio.");
+    let currentVault: IPassword[] = [];
+    let currentSalt: string | undefined;
 
-    const payload: EncryptedPayload = JSON.parse(content);
-    const currentVault = (await decryptVault(masterKey, payload)) as IPassword[];
+    const masterKeyBuffer = Buffer.from(masterKey, "base64");
+
+    if (content) {
+      const payload: EncryptedPayload = JSON.parse(content);
+      currentSalt = payload.salt;
+      currentVault = await decryptVault(masterKeyBuffer, payload);
+    } else {
+      throw new Error("Cofre não inicializado. Faça login novamente.");
+    }
 
     const newVault = currentVault.map((item) => (item.uuid === updatedItem.uuid ? updatedItem : item));
 
-    const newPayload = await encryptVault(masterKey, newVault);
+    const newPayload = await encryptVault(masterKeyBuffer, newVault, currentSalt);
 
     await writeVaultFile(JSON.stringify(newPayload));
-
     await syncVaultWithServer(newPayload);
 
     return true;
@@ -95,17 +111,24 @@ export const useEditPassword = async (masterKey: string, updatedItem: IPassword)
 export const useDeletePassword = async (masterKey: string, itemUuid: string) => {
   try {
     const content = await readVaultFile();
-    if (!content) throw new Error("Cofre não encontrado.");
+    let currentVault: IPassword[] = [];
+    let currentSalt: string | undefined;
 
-    const payload: EncryptedPayload = JSON.parse(content);
-    const currentVault = (await decryptVault(masterKey, payload)) as IPassword[];
+    const masterKeyBuffer = Buffer.from(masterKey, "base64");
+
+    if (content) {
+      const payload: EncryptedPayload = JSON.parse(content);
+      currentSalt = payload.salt;
+      currentVault = await decryptVault(masterKeyBuffer, payload);
+    } else {
+      throw new Error("Cofre não inicializado. Faça login novamente.");
+    }
 
     const newVault = currentVault.filter((item) => item.uuid !== itemUuid);
 
-    const newPayload = await encryptVault(masterKey, newVault);
+    const newPayload = await encryptVault(masterKeyBuffer, newVault, currentSalt);
 
     await writeVaultFile(JSON.stringify(newPayload));
-
     await syncVaultWithServer(newPayload);
 
     return true;
